aac599cb1373133266061a33fdfb202574f3ebfd
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-12 15:10:07 -0300

    add

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -5,7 +5,21 @@ exports.addToCart = async (req, res) => {
     let carrinho = await Cart.findOne();
     if(!carrinho) carrinho = new Cart({ produtos: []});
 
-    const existente = carrinho.produtos.find(p => p.produtoId.equals(produtoId))
+    const existente = carrinho.produtos.find(p => p.produtoId.equals(produtoId));
+
+    if(existente){
+        existente.quantidade += quantidade
+    } else {
+        carrinho.produtos.push({ produtoId, quantidade })
+    }
+
+    await carrinho.save();
+    res.json(carrinho);
 
 
+}
+
+exports.getCart = async (req, res) => {
+    const carrinho = await Cart.findOne().populate("produtos.produtoId");
+    
 }
