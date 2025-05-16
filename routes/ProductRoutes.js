10496727acd94ddc55e4d9121d1a82a2a4c17afa
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-16 15:09:21 -0300

    add

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {} = require("../controllers/")
+const { createProduct, getAllProducts} = require("../controllers/")
 const router = express.Router();
 
 router.post("/", createProduct);
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,4 +1,9 @@
 
+const express = require("express");
+const router = express.Router();
+const { addToCart, getCart} = require("../controllers/cartController")
 
 router.post("/", addToCart);
-router.get("/", getCart)
+router.get("/", getCart);
+
+module.exports = router;
