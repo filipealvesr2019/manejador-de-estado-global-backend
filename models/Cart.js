5e4f55a247952d1d3e72b8a4fea5c31b50c8f4eb
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-04 16:05:16 -0300

    add

--- models/Cart.js
+++ models/Cart.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
     produtos: [{
-        produtoId: { type: mongoose.Schema.Types.ObjectId, ref: "Product"},
+        produtoId: { type: mongoose.Schema.Types.ObjectId, ref: "Produto"},
         quantidade: Number
     }]
 });
